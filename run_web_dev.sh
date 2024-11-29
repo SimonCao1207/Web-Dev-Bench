0866b926a4886b0af6a7aed424791d8042d7ad78
SimonCao1207 <hainamkaist@gmail.com>
2024-11-29 22:18:42 +0700

    update demonstration in trajectory

--- run_web_dev.sh
+++ run_web_dev.sh
@@ -1,6 +1,6 @@
 python run.py \
   --data_path task_instance.md \
-  --repo_path /root/realworld-mask-update-user \ 
+  --repo_path /root/realworld-mask-update-user \
   --config_file config/default_from_url_web_dev.yaml \
   --model human \
   --environment_setup config/environment_setup/realworld_flask.sh \
